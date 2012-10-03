ca4301d41373681e04f4c3361db0b64daba1bae7
Will Farrington <wfarr@github.com>
2012-10-02 18:40:21 -0700

    Update manifests

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,18 +1,19 @@
 class rbenv {
-  require github::config
+  require boxen::config
 
-  $root = "${github::config::home}/rbenv"
+  $root = "${boxen::config::home}/rbenv"
 
   file { [$root, "${root}/versions"]:
     ensure => directory
   }
 
+  # we ensure latest of ruby-build for up-to-date ruby definitions
   package { ['ruby-build', 'rbenv']:
     ensure => latest
   }
 
-  file { "${github::config::envdir}/rbenv.sh":
+  file { "${boxen::config::envdir}/rbenv.sh":
     source  => 'puppet:///modules/rbenv/rbenv.sh',
-    require => File[$github::config::envdir]
+    require => File[$boxen::config::envdir]
   }
 }
